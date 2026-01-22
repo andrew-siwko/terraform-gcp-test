1a3ac6128ef328696fadd3492a6d06fc3163e78d
Andrew Siwko <asiwko@siwko.org>
2026-01-21 20:11:22 -0500

    Let's kick things off with a gcp provider and back end.

---
+++ 01-provider.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    # We will be working with GCP and so will need the google provider
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }    # in order to update DNS on linode, we'll need the linode provider.
+    linode = {
+      source  = "linode/linode"
+    }
+  }
+  # We want to store the Terraform state file in gcp using a GCS bucket.
+  backend "gcs" {
+    bucket  = "asiwko-gcp-terraform-state-bucket" # Name of your GCS bucket
+    prefix  = "terraform/state"                   # Path inside the bucket
+  }
+}
+
+provider "google" {
+  project = "helpful-weft-485019-h7"
+  region  = "us-east1"
+}
+variable "LINODE_API_KEY" {
+  description = "The key to the Linode API"
+  type        = string
+}
+
+provider "linode" {
+  token = var.LINODE_API_KEY
+}
