113c3ae2c4d8fff56eb70c683ba6f7c23fff2bb9
Andrew Siwko <asiwko@siwko.org>
2026-02-05 22:25:04 -0500

    Use ID not name to link firewall to network.

--- 04-firewall.tf
+++ 04-firewall.tf
@@ -1,6 +1,6 @@
 resource "google_compute_firewall" "allow-web-and-ssh" {
   name    = "allow-web-and-ssh"
-  network = google_compute_network.vpc_network.name
+  network = google_compute_network.vpc_network.id
 
   allow {
     protocol = "tcp"
