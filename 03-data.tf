69a4b78d7f4fcd66bb537b827d8ac155838a15ba
Andrew Siwko <asiwko@siwko.org>
2026-02-15 15:27:28 -0500

    filter the instances on low cpu and memory

--- 03-data.tf
+++ 03-data.tf
@@ -5,5 +5,6 @@ output "gcp_machine_types" {
   value = [
     for machine in data.google_compute_machine_types.available.machine_types :
     format("%s: CPU=%v, RAM=%vMB", machine.name, machine.guest_cpus, machine.memory_mb)
+    if machine.memory_mb <= 1024 && machine.guest_cpus <= 2
   ]
 }
