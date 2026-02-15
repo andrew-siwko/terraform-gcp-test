7a665d6bb58d8e484dd1826088de6bd9d197046f
Andrew Siwko <asiwko@siwko.org>
2026-02-15 15:21:18 -0500

    list out instances

--- 03-data.tf
+++ 03-data.tf
@@ -1,1 +1,9 @@
-03-data.tf
+data "google_compute_machine_types" "available" {
+  zone = var.zone
+}
+output "gcp_machine_types" {
+  value = [
+    for machine in data.google_compute_machine_types.available.machine_types :
+    format("%s: CPU=%v, RAM=%vMB", machine.name, machine.guest_cpus, machine.memory_mb)
+  ]
+}
