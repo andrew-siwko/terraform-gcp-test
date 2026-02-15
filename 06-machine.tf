ea638fd4537b31f311290d1bb1f4f42cc1c712e0
Andrew Siwko <asiwko@siwko.org>
2026-02-15 15:53:34 -0500

    change machine name; change tag name ; paramaterize user name

--- 06-machine.tf
+++ 06-machine.tf
@@ -1,15 +1,13 @@
 resource "google_compute_instance" "vm_instance" {
-  name         = "asiwko-vm-01"
-  machine_type = var.instance_type
+  name         = "vm-01"
+  machine_type = local.gcp_instance_type
   zone         = var.zone
 
-  tags = ["asiwko-vms"]
+  tags = ["vms"]
   
   metadata = {
-      # Replace 'your-username' with your local username
-      # Replace the string below with the contents of your ~/.ssh/id_rsa.pub
-      ssh-keys = "gcp-user:${file("/container_shared/ansible/ansible_rsa.pub")}"
-    }
+    ssh-keys = "${var.admin_username}:${file("/container_shared/ansible/ansible_rsa.pub")}"
+    }    
     allow_stopping_for_update = true
     boot_disk {
     initialize_params {
