99e1e45a66fa8c88aa412f256ec958388bc7b9aa
Andrew Siwko <asiwko@siwko.org>
2026-02-15 15:51:31 -0500

    move machine type output to output file; output image names

--- 03-data.tf
+++ 03-data.tf
@@ -1,10 +1,22 @@
 data "google_compute_machine_types" "available" {
   zone = var.zone
 }
-output "gcp_machine_types" {
-  value = [
-    for machine in data.google_compute_machine_types.available.machine_types :
-    format("%s: CPU=%v, RAM=%vMB", machine.name, machine.guest_cpus, machine.memory_mb)
-    if machine.memory_mb <= 1024 && machine.guest_cpus <= 2
+
+locals {
+  free_tier_machines = [
+    for machine in data.google_compute_machine_types.lab_discovery.machine_types : machine.name
+    if machine.name == "e2-micro"
   ]
+  gcp_instance_type = try(local.free_tier_machines[0], "e2-micro")
 }
+
+data "google_compute_image" "rhel_9" {
+  # The family for the standard RHEL 9 image
+  family  = "rhel-9"
+  project = "rhel-cloud"
+}
+
+output "rhel_9_images" {
+  value = datas.google_compute_image.rhel_9
+}
+
