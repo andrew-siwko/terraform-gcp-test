debcada707e29f141a6eb2caa151fb14386cfeec
Andrew Siwko <asiwko@siwko.org>
2026-02-15 15:53:45 -0500

    add machine types and tier

--- 08-outputs.tf
+++ 08-outputs.tf
@@ -2,3 +2,14 @@ output "google_instance_public_ip" {
   value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
 }
 
+output "gcp_machine_types" {
+  value = [
+    for machine in data.google_compute_machine_types.available.machine_types :
+    format("%s: CPU=%v, RAM=%vMB", machine.name, machine.guest_cpus, machine.memory_mb)
+    if machine.memory_mb <= 1024 && machine.guest_cpus <= 2
+  ]
+}
+
+output "free_tier_machine" {
+  value = local.gcp_instance_type
+}
