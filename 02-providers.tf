39025e91d6e49bdf7053d1a58cae085265aaee97
Andrew Siwko <asiwko@siwko.org>
2026-03-16 13:15:19 -0400

    moved the state to local storage

--- 02-providers.tf
+++ 02-providers.tf
@@ -9,11 +9,16 @@ terraform {
       source  = "linode/linode"
     }
   }
-  # We want to store the Terraform state file in gcp using a GCS bucket.
-  backend "gcs" {
-    bucket  = "asiwko-gcp-terraform-state-bucket" # Name of your GCS bucket
-    prefix  = "terraform/state"                   # Path inside the bucket
+   backend "local" {
+    path = "/container_shared/tfstate/aws.tfstate"
   }
+
+  # This project started with the state stored in the provider's oject storage.  
+  # I moved it to local storage as providers charge for object storage and there was no benefit once the exercise was complete.
+  # backend "gcs" {
+  #   bucket  = "asiwko-gcp-terraform-state-bucket" # Name of your GCS bucket
+  #   prefix  = "terraform/state"                   # Path inside the bucket
+  # }
 }
 
 provider "google" {
